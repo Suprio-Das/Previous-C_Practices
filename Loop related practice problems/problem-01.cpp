6c380dcc2e9bbc1848024895f75195b747f13097
Suprio-Das <priyamd507@gmail.com>
2025-03-01 23:28:43 +0600

    Loop related practice problems

---
+++ Loop related practice problems/problem-01.cpp
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include<string.h>
+
+int main() {
+    char input[100];
+    
+    scanf("%c",&input); // Input: I love programming
+    
+    printf("%c\n",input);
+
+    return 0;
+}
